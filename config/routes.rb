916953406a9b5f351bbc8dc191d6aa34e6af2710
German Renato Cobian <germancobian@hotmail.com>
2022-05-30 10:05:35 -0700

    Create api with greetings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static#index'
+  namespace :api, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
+end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
