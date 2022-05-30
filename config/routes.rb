8a50163c88840189ef61a72697adc6fbe9f2a5f8
German Renato Cobian <germancobian@hotmail.com>
2022-05-30 11:16:09 -0700

    Initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  root 'static#index'
-  namespace :api, defaults: { format: 'json' } do
-    get 'greetings', to: 'greetings#index'
-  end
-end
-
-# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+end
