362d46e9d7d6824e3ce8d43f56eccb64440ca13e
German Renato Cobian <germancobian@hotmail.com>
2022-05-30 13:15:01 -0700

    Use react-router

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     get 'greetings', to: 'greetings#index'
   end
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
